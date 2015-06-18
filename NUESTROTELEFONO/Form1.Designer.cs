85b04ba2642828e27f5ee4800d27cb91ee0f1a98
aleramirezsj <aleramirezsj@gmail.com>
2015-06-18 19:52:15 -0300

    Primera versión funcional que envia mensajes

---
+++ NUESTROTELEFONO/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NUESTROTELEFONO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbPuertos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbPuertos
+            // 
+            this.cmbPuertos.FormattingEnabled = true;
+            this.cmbPuertos.Location = new System.Drawing.Point(117, 48);
+            this.cmbPuertos.Name = "cmbPuertos";
+            this.cmbPuertos.Size = new System.Drawing.Size(136, 21);
+            this.cmbPuertos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CMD Port";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(31, 97);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefono.TabIndex = 2;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(31, 133);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(47, 13);
+            this.lblMensaje.TabIndex = 3;
+            this.lblMensaje.Text = "Mensaje";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(117, 94);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(135, 20);
+            this.txtTelefono.TabIndex = 4;
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(117, 136);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(144, 20);
+            this.txtMensaje.TabIndex = 5;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(80, 193);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(106, 24);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPuertos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPuertos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ NUESTROTELEFONO/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace NUESTROTELEFONO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GSM sm = new GSM(cmbPuertos.Text);
+            sm.Opens();
+            sm.sendSms(txtTelefono.Text, txtMensaje.Text);
+            sm.Closes();
+            MessageBox.Show("Mensaje Enviado!!!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            foreach (string puerto in puertos)
+            {
+                cmbPuertos.Items.Add(puerto);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
